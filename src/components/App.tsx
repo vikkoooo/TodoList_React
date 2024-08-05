6f849e27d526771006d060b2edb38eec5cd8e804
vikkoooo <vikkolundberg@gmail.com>
2024-08-05 13:11:42 +0200

    cleanup import font and material symbols

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+export function App() {
+	return (
+		<>
+			<h1>This is the App component</h1>
+			<span className="material-symbols-outlined">
+				settings
+			</span>
+		</>
+	);
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
+import { App } from "./components/App.tsx";
 
-import "./index.css";
+import "./css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
