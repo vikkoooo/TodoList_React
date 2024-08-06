b098c24f0a3919fbf340ecee352950266cfe8add
vikkoooo <vikkolundberg@gmail.com>
2024-08-06 16:05:25 +0200

    style todo list

--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -9,12 +9,13 @@ interface TodoTaskProps {
 
 export function TodoTask(props: TodoTaskProps): ReactElement {
 	return (
-		<li><span className="timestamp">{props.todoTask.timestamp.toLocaleString()}</span>
-			<span className="task-name">{props.todoTask.task}</span>
-			<span className="task-author">{props.todoTask.author}</span>
+		<li className="task-item">
 			{props.todoTask.isCompleted
 				? <span className="material-symbols-outlined is-completed" onClick={() => props.handleTaskClick(props.todoTask)}>task_alt</span>
 				: <span className="material-symbols-outlined is-completed" onClick={() => props.handleTaskClick(props.todoTask)}>circle</span>}
+			<span className="timestamp">{props.todoTask.timestamp.toLocaleString()}</span>
+			<span className="task-author">{props.todoTask.author}</span>
+			<span className="task">{props.todoTask.task}</span>
 			<span className="material-symbols-outlined delete" onClick={() => props.handleDeleteClick(props.todoTask)}>delete</span>
 		</li >
 	);
