92f34d178f78f497bccbfc6fb0cdc40e08ea8038
vikkoooo <vikkolundberg@gmail.com>
2024-08-06 16:24:23 +0200

    correctly fix timestamp and author being on one line

--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -13,9 +13,15 @@ export function TodoTask(props: TodoTaskProps): ReactElement {
 			{props.todoTask.isCompleted
 				? <span className="material-symbols-outlined is-completed" onClick={() => props.handleTaskClick(props.todoTask)}>task_alt</span>
 				: <span className="material-symbols-outlined is-completed" onClick={() => props.handleTaskClick(props.todoTask)}>circle</span>}
-			<span className="timestamp">{props.todoTask.timestamp.toLocaleString()}</span>
-			<span className="task-author">{props.todoTask.author}</span>
-			<span className="task">{props.todoTask.task}</span>
+			<div className="task-container">
+				<div className="upper-row">
+					<span className="timestamp">{props.todoTask.timestamp.toLocaleString()}</span>
+					<span className="task-author">{props.todoTask.author}</span>
+				</div>
+				<div className="lower-row">
+					<span className="task">{props.todoTask.task}</span>
+				</div>
+			</div>
 			<span className="material-symbols-outlined delete" onClick={() => props.handleDeleteClick(props.todoTask)}>delete</span>
 		</li >
 	);
