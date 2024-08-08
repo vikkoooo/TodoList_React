03c105cdd1ded65296c8ef5e4e99ea5537049017
vikkoooo <vikkolundberg@gmail.com>
2024-08-08 14:41:19 +0200

    the most ninja solution to editing todos

--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -1,25 +1,48 @@
-import { ReactElement } from "react";
+import { FormEvent, ReactElement, useState } from "react";
 import { TodoTask } from "../components/TodoTask";
 import { ITodo, ITodoContext } from "../interfaces";
 import { useOutletContext } from "react-router-dom";
 
+let todoGlobal: ITodo; // tmp storage
+
 export function TodoListPage(): ReactElement {
 	const { taskList, handleTaskClick, handleDeleteClick } = useOutletContext<ITodoContext>(); // context variables instead of interface and prop
+	const [newTaskInput, setNewTaskInput] = useState("");
+	const [newAuthorInput, setNewAuthorInput] = useState("");
 
 	const handleEditClick = (todo: ITodo) => {
-		console.log("edit clicked..");
+		setNewTaskInput(todo.task);
+		setNewAuthorInput(todo.author)
+		todoGlobal = todo; // store the todo object in our global variable
+	};
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		todoGlobal.task = newTaskInput;
+		todoGlobal.author = newAuthorInput;
+		todoGlobal.timestamp = new Date();
+		setNewTaskInput("");
+		setNewAuthorInput("");
 	};
 
 	return (
-		<ol className="task-list">
-			{taskList.map((element) => (
-				<TodoTask
-					key={element.id}
-					todoTask={element}
-					handleTaskClick={handleTaskClick}
-					handleDeleteClick={handleDeleteClick}
-					handleEditClick={handleEditClick} />
-			))}
-		</ol>
+		<div className="todo-list-page">
+			<ol className="task-list">
+				{taskList.map((element) => (
+					<TodoTask
+						key={element.id}
+						todoTask={element}
+						handleTaskClick={handleTaskClick}
+						handleDeleteClick={handleDeleteClick}
+						handleEditClick={handleEditClick} />
+				))}
+			</ol>
+
+			<form className="edit" onSubmit={handleSubmit}>
+				<input type="text" className="edit-task-field" placeholder="Edit Task" value={newTaskInput} onChange={(event) => setNewTaskInput(event.target.value)} />
+				<input type="text" className="edit-author-field" placeholder="Edit Author" value={newAuthorInput} onChange={(event) => setNewAuthorInput(event.target.value)} />
+				<button type="submit" className="edit-button">Edit</button>
+			</form>
+		</div>
 	);
 }
